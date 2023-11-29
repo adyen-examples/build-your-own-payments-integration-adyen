3b20e942594cb050f3211819445d7e5c92809a38
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-11-29 15:40:47 +0100

    Add idempotency-key

--- src/main/java/com/adyen/checkout/api/CheckoutResource.java
+++ src/main/java/com/adyen/checkout/api/CheckoutResource.java
@@ -7,6 +7,7 @@ import com.adyen.checkout.services.CartService;
 import com.adyen.checkout.services.DonationService;
 import com.adyen.checkout.services.OrderService;
 import com.adyen.enums.Environment;
+import com.adyen.model.RequestOptions;
 import com.adyen.model.checkout.*;
 import com.adyen.service.checkout.PaymentsApi;
 import com.adyen.service.exception.ApiException;
@@ -129,8 +130,11 @@ public class CheckoutResource {
             paymentRequest.setOrderReference(body.getOrderReference());
         }
 
+        var requestOptions = new RequestOptions();
+        requestOptions.setIdempotencyKey(UUID.randomUUID().toString());
+
         log.info("REST request to make Adyen payment {}", paymentRequest);
-        var response = paymentsApi.payments(paymentRequest);
+        var response = paymentsApi.payments(paymentRequest, requestOptions);
 
         // When a successful response, we set the remaining amount correctly in the orderService.
         if (response.getOrder() != null) {
